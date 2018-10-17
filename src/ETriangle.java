035a5bda7dd2a16170fe414dac93a40577b2e947
BT_1N3_02@BKTECH.LOCAL <BT_1N3_02@BKTECH.LOCAL>
2018-10-17 08:40:31 -0400

    Initial commit

---
+++ src/ETriangle.java
@@ -0,0 +1,19 @@
+public class ETriangle extends Shape {
+   private int side;
+   public ETriangle( int side){
+       super("ETriangle");
+       this.side = side;
+   }
+   @Override
+    public double getArea()
+   {
+       return (Math.sqrt(3)/4)*(side*side);
+   }
+   @Override
+    public double getPerimeter()
+   {
+       return side*3;
+   }
+
+
+}
